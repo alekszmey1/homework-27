d4c592df353994ef0cda08a815a8bd343ab8431d
alekszmey1 <86463231+alekszmey1@users.noreply.github.com>
2021-09-29 16:03:41 +0700

    вторая попытка

--- main.go
+++ main.go
@@ -7,46 +7,40 @@ import (
 	"strconv"
 	"strings"
 )
-
-/*Напишите программу, которая считывает ввод с stdin,
-создаёт структуру student и записывает указатель на
-структуру в хранилище map[studentName] *Student.
-Программа должна получать строки в бесконечном цикле, создать структуру Student через функцию newStudent,
-далее сохранить указатель на эту структуру в map, а после получения EOF (ctrl + d)
-вывести на экран имена всех студентов из хранилища. Также необходимо реализовать методы put, get.
- */
-
-type student struct {
+type Student struct {
 	name string
 	age int
 	grade int
 }
 
-func newStudent(a string)(p student){
-	s := strings.Split(a," ")
-	for i := 0; i < len(s); i++ {
-		if i == 0 {
-			p.name = s[i]
-			fmt.Println(p.name)
+func main() {
+	scanner:=bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	m := make(map[int]*Student)
+	k:=1
+	for scanner.Scan() {
+		line := scanner.Text()
+		s:= strings.Split(line," ")
+		p:= Student{}
+		for i := 0; i < len(s); i++ {
+			if i == 0 {
+				p.name = s[i]
+			}
+			if i == 1{
+				p.age,_ = strconv.Atoi(s[i])
+			}
+			if i == 2{
+				fmt.Println(s[i])
+				p.grade ,_ = strconv.Atoi(s[i])
+			}
+
 		}
-		if i == 1{
-			p.age,_ = strconv.Atoi(s[i])
-			fmt.Println(p.age)
-		}
-		if i == 2{
-			fmt.Println(s[i])
-			p.grade ,_ = strconv.Atoi(s[i])
-			fmt.Println(p.grade)
-		}
+		fmt.Println("-------------------")
 		fmt.Println(p)
+		m = map[int]*Student{
+			k: *p,
+		}
+		k++
 	}
-	return p
-}
-
-
-
-
-func main()  {
-	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	newStudent(text)
+	fmt.Println(m)
 }
